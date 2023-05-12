a58991ab2a0ca82e21fc23288e40a13dec43504f
Ruslan <im.ruslan@mail.ru>
2023-05-12 23:19:58 +0700

    Рандомный цвет вместо контекстного меню

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 import './styles.css'
+import { ContextMenu } from "./menu";
 
+const context = new ContextMenu();
+document.body.addEventListener('contextmenu',(e)=>{
+    e.preventDefault();
+    context.open();
+      
+})
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,9 @@
-import {Menu} from './core/menu'
-
+import { Menu } from "./core/menu";
+import { getRandomColor } from "./utils";
 export class ContextMenu extends Menu {
 
+    open(){
+        
+        document.body.style.backgroundColor = getRandomColor();
+    }
 }
