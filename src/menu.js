8183031506a320b244093eb38ac7d6cdd17d371f
Ruslan <im.ruslan@mail.ru>
2023-05-14 18:33:16 +0700

    Добавил сабменю(полурабочее)

--- src/menu.js
+++ src/menu.js
@@ -2,15 +2,27 @@ import { Menu } from "./core/menu";
 import { BackgroundModule } from "./modules/background.module";
 import { ClicksModule } from "./modules/clicks.module";
 import { ExitModule } from "./modules/exit.module";
+import { DevelopersModule } from "./modules/developers.module";
 export class ContextMenu extends Menu {
   constructor() {
     super(".menu");
     this.background = new BackgroundModule("background", "Поменять цвет");
     this.clicks = new ClicksModule("clicks", "Посчитать клики");
     this.exit = new ExitModule("exit", "Poof!");
+    this.developers = new DevelopersModule("developers", "Разработчики");
     this.el.addEventListener("click", (e) => {
-      this[e.target.dataset.type].trigger();
-      this.close();
+      if (e.target.dataset.type !== "developers") {
+        this[e.target.dataset.type].trigger();
+        this.close();
+      }
+    });
+    document.body.addEventListener("mouseover", (e) => {
+      if (e.target.dataset.type === "developers") this.developers.trigger();
+    });
+    document.body.addEventListener("mouseout", (e) => {
+      if (e.target.dataset.type === "developers") {
+        this.developers.close();
+      }
     });
   }
   close() {
@@ -28,6 +40,7 @@ export class ContextMenu extends Menu {
     this.add(this.background);
     this.add(this.clicks);
     this.add(this.exit);
+    this.add(this.developers);
   }
 
   add(moduleName) {
