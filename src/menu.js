25d6769531dded5e2d12dc19f080f5199990a4c7
Kirill <kirillmaznov@gmail.com>
2023-05-14 11:51:20 +0300

    разработал модуль показа рандомного сообщения, внес правки под модуль в стили, утилс

--- src/menu.js
+++ src/menu.js
@@ -2,12 +2,18 @@ import { Menu } from "./core/menu";
 import { BackgroundModule } from "./modules/background.module";
 import { ClicksModule } from "./modules/clicks.module";
 import { ExitModule } from "./modules/exit.module";
+import { RandomPhraseModule } from "./modules/random.phrase.module";
+
 export class ContextMenu extends Menu {
   constructor() {
     super(".menu");
     this.background = new BackgroundModule("background", "Поменять цвет");
     this.clicks = new ClicksModule("clicks", "Посчитать клики");
     this.exit = new ExitModule("exit", "Poof!");
+    this.randomPhrase = new RandomPhraseModule(
+      "randomPhrase",
+      "Генератор фраз"
+    );
     this.el.addEventListener("click", (e) => {
       this[e.target.dataset.type].trigger();
       this.close();
@@ -27,6 +33,7 @@ export class ContextMenu extends Menu {
 
     this.add(this.background);
     this.add(this.clicks);
+    this.add(this.randomPhrase);
     this.add(this.exit);
   }
 
