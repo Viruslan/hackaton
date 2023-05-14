cd69c50361870b79097004421495129483cf86ac
Andrey <saxarcor4@gmail.com>
2023-05-14 15:47:02 +0300

    add:clicksModule

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -6,18 +6,21 @@ import {Module} from '../core/module'
 export class ClicksModule extends Module {
     constructor(type, text){
         super(type, text)
+        this.input = document.createElement('input');
+        this.input.name = 'countTime';
+        this.input.type = 'number';
+        this.input.className=('selectTime');
 
-        this.input = document.createElement('input')
-        this.form = document.createElement('form')
-        this.button = document.createElement('button')
-        // this.input.setAttribute('name','countTime')
-        this.input.name = 'countTime'
-        this.input.type = 'number'
-        this.button.type = 'submit'
-        this.button.textContent= 'Go'
-        this.form.append(this.input, this.button)
-        this.spanRes = document.createElement('div')
-        this.body = document.body
+        this.button = document.createElement('button');
+        this.button.type = 'submit';
+        this.button.textContent= 'Начать';
+
+        this.form = document.createElement('form');
+        this.form.append(this.input, this.button);
+
+        this.spanRes = document.createElement('div');
+        this.spanRes.className= ('spanResult')
+        this.body = document.body;
     }
     trigger(){
         this.spanRes?.remove()
@@ -37,19 +40,12 @@ export class ClicksModule extends Module {
         this.body.addEventListener('click',(e)=>{
             count++
             this.counterDiv.textContent = `Количество кликов за ${countTime} секунд: ${count}`;
-            console.log(res)
         })
         const res = setTimeout(()=>{
             this.counterDiv.remove()
-            // console.log(count)
-            console.log('fin')
             this.spanRes.textContent = `Ваш результат ${count} кликов`
             count=0;
-        },countTime*1000)
-        // this.spanRes.textContent = `Ваш результат ${count} кликов`
-        setTimeout(()=>{
             this.body.append(this.spanRes)
-        },countTime*1001)
-        console.log(countTime)
+        },countTime*1000)
     }
 }
