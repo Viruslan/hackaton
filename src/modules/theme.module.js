42b94ca444f41a8d41fcc3ac7e13519b25d623e5
gurushchuk <hanzlibnik@gmail.com>
2023-05-14 20:33:37 +0200

    added light theme for submenu and buttons

--- src/modules/theme.module.js
+++ src/modules/theme.module.js
@@ -2,26 +2,27 @@ import { Module } from '../core/module';
 
 export class ThemeModule extends Module {
   
-  #menu;
-  
+  #style;
+  #css;
   #isDarkTheme;
 
   constructor(type, text) {
     super(type, text);
-    this.#menu = document.querySelector('.menu');
     this.#isDarkTheme = true;
+    this.#style = document.createElement('style');
+    this.#css = '.submenu {background: #f7f7f7; color: #525252; border: 1px solid #9d9d9d}'
+      + ' .menu {background: #f7f7f7; color: #525252; border: 1px solid #9d9d9d}' 
+      + ' .start {background-color: #f7f7f7; border: 1px solid #9d9d9d; color: #525252;}' 
+      + ' .selectTimeButton {background: #f7f7f7; border: 1px solid #9d9d9d; color: #525252;};';
   }
 
   trigger() {
     this.#isDarkTheme = !this.#isDarkTheme;
     if (!this.#isDarkTheme) {
-      this.#menu.style.background = '#f7f7f7';
-      this.#menu.style.border = '1px solid #9d9d9d';
-      this.#menu.style.color = '#525252';
+      this.#style.appendChild(document.createTextNode(this.#css));
+      document.querySelector('head').append(this.#style);
     } else {
-      this.#menu.style.background = '#1b1a1a';
-      this.#menu.style.border = 'none';
-      this.#menu.style.color = '#eee';
+      this.#style.remove();
     }
   }
 }
