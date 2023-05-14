8688d6b5dae0f91fd6aad5aec2b71c8da81da533
Andrey <saxarcor4@gmail.com>
2023-05-14 19:12:35 +0300

    fix

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,7 +1,4 @@
 import {Module} from '../core/module'
-// const span = document.createElement('span');
-// span.className('clicks')
-
 
 export class ClicksModule extends Module {
     constructor(type, text){
@@ -10,32 +7,38 @@ export class ClicksModule extends Module {
         this.input.name = 'countTime';
         this.input.type = 'number';
         this.input.className=('selectTime');
+        this.input.setAttribute('placeholder','Введите количество секунд')
 
         this.button = document.createElement('button');
         this.button.type = 'submit';
         this.button.textContent= 'Начать';
+        this.button.className=('selectTimeButton');
 
         this.form = document.createElement('form');
+        this.form.className=('modal-content')
         this.form.append(this.input, this.button);
-
+        this.modalBackground = document.createElement('div');
+        this.modalBackground.className=('modal')
+        this.modalBackground.append(this.form)
         this.spanRes = document.createElement('div');
         this.spanRes.className= ('spanResult')
         this.body = document.body;
+
     }
     trigger(){
         this.spanRes?.remove()
-        this.body.append(this.form)
+        this.body.append(this.modalBackground)
         this.form.addEventListener('submit',(e)=>{
             e.preventDefault()
             let countTime = Number(e.target.elements.countTime.value)
-            this.form.remove()
+            this.modalBackground.remove()
             this.counter(countTime)
         })
     }
     counter(countTime){
-        this.counterDiv = document.createElement('div')
-        this.counterDiv.className = ('countd')
         let count = 0
+        this.counterDiv = document.createElement('div')
+        this.counterDiv.className = ('counterDiv')
         this.body.append(this.counterDiv)
         this.body.addEventListener('click',(e)=>{
             count++
@@ -43,9 +46,12 @@ export class ClicksModule extends Module {
         })
         const res = setTimeout(()=>{
             this.counterDiv.remove()
-            this.spanRes.textContent = `Ваш результат ${count} кликов`
+            this.spanRes.textContent = `Ваш результат ${count} кликов!`
             count=0;
             this.body.append(this.spanRes)
+            setTimeout(()=>{
+                this.spanRes.remove()
+            },3000)
         },countTime*1000)
     }
 }
